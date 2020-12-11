e1f574cb029621e9ad80fc9f24cf3d4d57d895e2
swapan <sdbit04@gmail.com>
2020-12-11 13:01:52 +0530

    Corrected Messages, added seq-no as primary key of data_dict from each file

--- message_counter.py
+++ message_counter.py
@@ -5,73 +5,89 @@ from openpyxl import Workbook
 from tkinter import Tk, Entry, Button, Label, messagebox
 
 
-def count_occurrences_of_string(search_string, input_file_path):
-    _in_str = "value\s+\({}\)".format(search_string)
+def count_occurrences_of_string(input_file_path):
+    _in_str = "^\s*(value)?\s*[(]*[m,M]ME[-_]UE[-_]S1AP[-_]ID[)]*\s*=\s*\d*"
     with open("Search_MMS-UE-S1AP-ID.log", "a") as log_ob:
-        print(_in_str, file=log_ob)
+        print("Searching for: 'value (MME-UE-S1AP-ID) =', and 'mME_UE_S1AP_ID =' ", file=log_ob)
     _in_str_pat = re.compile(_in_str)
-    add_enodebe_id = "ENODEB ID:"
-    add_date_time = "TIME AND DATE:"
-    add_time_stamp = "TIMESTAMP:"
-    add_event_name = "MESSAGE TYPE:"
+    add_sequence_no = re.compile("^SEQUENCE\s{0,2}NUMBER:")
+    add_utran_trace_id = re.compile("^EUTRAN\s{0,2}TRACE\s{0,2}ID:")
+    add_enodebe_id = re.compile("^ENODEB\s{0,2}ID:")
+    add_date_time = re.compile("^TIME\s{0,2}AND\s{0,2}DATE:")
+    add_cell_id = re.compile("^CELL\s{0,2}ID:")
+    add_message_type = re.compile("^MESSAGE\s{0,2}TYPE:")
     with open(input_file_path, 'r') as input_file_ob:
         lines = input_file_ob.readlines()
         uplink_id_v_data = {}
-        for ind, line in enumerate(lines):
-            if re.search(_in_str_pat, line):
-                uplink_id = line.strip()
-                enodeb_id = None
-                date_time = None
-                time_stamp = None
-                event_name = None
-                for back_ind in range(ind, ind-100, -1):
-                    back_line = lines[back_ind]
-                    if re.search(add_enodebe_id, back_line):
-                        enodeb_id = back_line.split(":")[1].strip()
-                    elif re.search(add_date_time, back_line):
-                        date_time = back_line.split(":")[1].strip()
-                    elif re.search(add_time_stamp, back_line):
-                        time_stamp = back_line.split(":")[1].strip()
-                    elif re.search(add_event_name, back_line):
-                        event_name = back_line.split(":")[1].strip()
-                uplink_id_v_data[uplink_id]=[enodeb_id, date_time, time_stamp,event_name]
-
+    for ind, line in enumerate(lines):
+            # if re.search(_in_str_pat, line):
+        line_match = _in_str_pat.match(line)
+        # if _in_str_pat.match(line) is not None:
+        if line_match is not None:
+            uplink_id = line_match.group(0).strip()
+            with open("Search_MMS-UE-S1AP-ID.log", "a") as log_ob:
+                print("{}-{}".format(ind, line_match.group(0)), file=log_ob)
+                message_sequence_no = None; utran_trace_id = None; enodeb_id = None; date_time = None; cell_id = None; message_type = None
+            for back_ind in range(ind, ind-200, -1):
+                back_line = lines[back_ind]
+                if add_message_type.search(back_line):
+                    message_type = back_line.split(":", maxsplit=1)[1].strip()
+                elif add_cell_id.search(back_line):
+                    cell_id = back_line.split(":", maxsplit=1)[1].strip()
+                elif add_enodebe_id.search(back_line):
+                    enodeb_id = back_line.split(":", maxsplit=1)[1].strip()
+                elif add_date_time.search(back_line):
+                    date_time = back_line.split(":", maxsplit=1)[1].strip()
+                elif add_utran_trace_id.search(back_line):
+                    utran_trace_id = back_line.split(":", maxsplit=1)[1].strip()
+                elif add_sequence_no.match(back_line):
+                    message_sequence_no = back_line.split(":", maxsplit=1)[1].strip()
+                    break
+            uplink_id_v_data[message_sequence_no]=[uplink_id, utran_trace_id, enodeb_id, date_time, cell_id, message_type]
     return uplink_id_v_data
 
 
-def count_occurrence_in_dir(search_string, input_dir_path):
+def count_occurrence_in_dir(input_dir_path):
     filename_v_uplink = {}
     for cur_dir, dirs, files in os.walk(input_dir_path):
         for input_file in files:
             with open("Search_MMS-UE-S1AP-ID.log", "a") as log_ob:
                 print("Reading file = {}".format(input_file), file=log_ob)
             input_file_path = os.path.join(cur_dir, input_file)
-            _uplink_id_v_data = count_occurrences_of_string(search_string, input_file_path)
+            # _uplink_id_v_data = count_occurrences_of_string(input_file_path)
+            _seq_no_v_data = count_occurrences_of_string(input_file_path)
+            # with open("Search_MMS-UE-S1AP-ID.log", "a") as log_ob:
+            #     print(_uplink_id_v_data, file=log_ob)
 
-            if _uplink_id_v_data is not None:
-                filename_v_uplink[input_file] = _uplink_id_v_data
+            if _seq_no_v_data is not None:
+                filename_v_uplink[input_file] = _seq_no_v_data
             else:
                 with open("Search_MMS-UE-S1AP-ID.log", "a") as log_ob:
-                    print("\t No match for {} found in this file".format(search_string), file=log_ob)
+                    print("\t No match in {} found in this file".format(input_file), file=log_ob)
     else:
         return filename_v_uplink
+# Got this file_v_uplink dict uptodate 10-Dec
 
 
 def write_to_excel(file_v_uplink_and_related_data):
     time_part = time.strftime("%d-%b-%Y-%H%M%S", time.gmtime())
     ouput_file_path = "output_{}.xlsx".format(time_part)
     WB = Workbook()
-    sheet_ob = WB.create_sheet()
-    headers = ["File_name", "UPLINK_ID", "ENODB_ID", "DATE-TIME", "TIMESTAMP", "EVENT_NAME"]
+    sheet_ob = WB.worksheets[0]
+    sheet_ob.title = "Message_Counter"
+    # sheet_ob = WB.create_sheet()
+    # [message_sequence_no] = [uplink_id, utran_trace_id, enodeb_id, date_time, cell_id, message_type]
+    headers = ["File_name", "UPLINK_ID", "UTRAN_TRACE_ID",  "ENODB_ID", "DATE-TIME", "CELL_ID", "MESSAGE_TYPE"]
     for ind, head in enumerate(headers, 1):
         sheet_ob.cell(row=1, column=ind, value=head)
     start_row = 1
     for file_key, value in file_v_uplink_and_related_data.items():
         for search_key, add_vaules in value.items():
+            # seach_key is message_sequence_no
             start_row += 1
             sheet_ob.cell(row=start_row, column=1, value=file_key)
-            sheet_ob.cell(row=start_row, column=2, value=search_key)
-            for add_value_ind, add_value in enumerate(add_vaules, start=3):
+            # sheet_ob.cell(row=start_row, column=2, value=search_key)
+            for add_value_ind, add_value in enumerate(add_vaules, start=2):
                 sheet_ob.cell(row=start_row, column=add_value_ind, value=add_value)
 
     WB.save(ouput_file_path)
@@ -91,8 +107,6 @@ def get_input():
     e2.grid(row=2, column=1)
 
     # messagebox_1 =
-
-
 
     def my_click():
         global input_string
@@ -123,13 +137,13 @@ if __name__ == "__main__":
     # input_directory = arguments.input_dir
     # print("Searching for " + search_string)
 
-    search_string, input_directory = get_input()
+    search_string_out, input_directory = get_input()
     print("Process has been started, Minimize this terminal. And check the Search_MMS-UE-S1AP-ID.log file, at current directory for details")
     with open("Search_MMS-UE-S1AP-ID.log", "w") as log_ob:
         print("Starting to search MMS-UE-#-ID at {} directory".format(input_directory), file=log_ob)
     # search_string = "MME-UE-S1AP-ID"
     # input_directory = r"D:\D_drive_BACKUP\MENTOR\Utility\Search_a_string\New folder"
-    filename_v_uplink_and_related_data = count_occurrence_in_dir(search_string, input_directory)
+    filename_v_uplink_and_related_data = count_occurrence_in_dir(input_directory)
     # print("filename_v_uplink_and_related_data = {}".format(filename_v_uplink_and_related_data))
     write_to_excel(filename_v_uplink_and_related_data)
 
